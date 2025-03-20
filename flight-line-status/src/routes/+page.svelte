763989aa84ddb5b4704ac18b21351be04578159f
andrehebra <52555960+andrehebra@users.noreply.github.com>
2025-03-20 16:32:57 +0000

    added flowbite-svelte and moved components over to it

--- flight-line-status/src/routes/+page.svelte
+++ flight-line-status/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { Heading, P } from 'flowbite-svelte';
+    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
 
 async function getCurrentMetarValues() {
 
@@ -6,5 +8,5 @@ async function getCurrentMetarValues() {
 
 </script>
 
-<h1>FLIGHT LINE STATUS</h1>
-<p>DATE / TIME LAST UPDATED</p>
+<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">FLIGHT LINE STATUS</Heading>
+<P>DATE / TIME LAST UPDATED</P>
