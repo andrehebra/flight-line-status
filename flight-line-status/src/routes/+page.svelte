6e89b558149b3afe400d5a48ed342aa86e2a77cf
andrehebra <52555960+andrehebra@users.noreply.github.com>
2025-03-20 17:00:26 +0000

    got table layout correct

--- flight-line-status/src/routes/+page.svelte
+++ flight-line-status/src/routes/+page.svelte
@@ -2,6 +2,27 @@
     import { Heading, P } from 'flowbite-svelte';
     import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Checkbox, TableSearch } from 'flowbite-svelte';
 
+    // variables to indicate whether or not flights are allowed: false = no go, true = go
+    let dualTrafficPattern = false;
+    let dualPracticeArea = false;
+    let dualCrossCountry = false;
+    let dualIFR = false;
+
+    let soloTrafficPattern = false;
+    let soloPracticeArea = false;
+    let soloCrossCountry = false;
+    let soloIFR = false;
+
+    let renterTrafficPattern = false;
+    let renterPracticeArea = false;
+    let renterCrossCountry = false;
+    let renterIFR = false;
+
+    let timeBuildTrafficPattern = false;
+    let timeBuildPracticeArea = false;
+    let timeBuildCrossCountry = false;
+    let timeBuildIFR = false;
+
 async function getCurrentMetarValues() {
 
 }
@@ -10,3 +31,105 @@ async function getCurrentMetarValues() {
 
 <Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">FLIGHT LINE STATUS</Heading>
 <P>DATE / TIME LAST UPDATED</P>
+
+<div class="tableContainer">
+<Table class="statusTable">
+    <TableHead>
+      <TableHeadCell>Flight Profile</TableHeadCell>
+      <TableHeadCell>Conditions</TableHeadCell>
+      <TableHeadCell>Status</TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+        <TableBodyRow>
+            <TableBodyCell rowspan="4">DUAL</TableBodyCell>
+            <TableBodyCell>Traffic Pattern</TableBodyCell>
+            <TableBodyCell>{dualTrafficPattern ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Practice Area</TableBodyCell>
+            <TableBodyCell>{dualPracticeArea ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Cross Country</TableBodyCell>
+            <TableBodyCell>{dualCrossCountry ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>IFR</TableBodyCell>
+            <TableBodyCell>{dualIFR ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+    <TableBody tableBodyClass="divide-y">
+        <TableBodyRow>
+            <TableBodyCell rowspan="4">SOLO</TableBodyCell>
+            <TableBodyCell>Traffic Pattern</TableBodyCell>
+            <TableBodyCell>{soloTrafficPattern ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Practice Area</TableBodyCell>
+            <TableBodyCell>{soloPracticeArea ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Cross Country</TableBodyCell>
+            <TableBodyCell>{soloCrossCountry ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>IFR</TableBodyCell>
+            <TableBodyCell>{soloIFR ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+    <TableBody tableBodyClass="divide-y">
+        <TableBodyRow>
+            <TableBodyCell rowspan="4">RENTER</TableBodyCell>
+            <TableBodyCell>Traffic Pattern</TableBodyCell>
+            <TableBodyCell>{renterTrafficPattern ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Practice Area</TableBodyCell>
+            <TableBodyCell>{renterPracticeArea ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Cross Country</TableBodyCell>
+            <TableBodyCell>{renterCrossCountry ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>IFR</TableBodyCell>
+            <TableBodyCell>{renterIFR ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+    <TableBody tableBodyClass="divide-y">
+        <TableBodyRow>
+            <TableBodyCell rowspan="4">TIME BUILD</TableBodyCell>
+            <TableBodyCell>Traffic Pattern</TableBodyCell>
+            <TableBodyCell>{timeBuildTrafficPattern ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Practice Area</TableBodyCell>
+            <TableBodyCell>{timeBuildPracticeArea ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Cross Country</TableBodyCell>
+            <TableBodyCell>{timeBuildCrossCountry ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>IFR</TableBodyCell>
+            <TableBodyCell>{timeBuildIFR ? "GO" : "NO-GO"}</TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+    
+  </Table>
+</div>
+
+<style>
+    :global(.statusTable) {
+        width: 100%;
+        border: 1px solid #ccc;
+    }
+
+    :global(.tableContainer) {
+        padding: 25px;
+    }
+
+    :global(TableBodyCell) {
+        text-align: center;
+    }
+</style>
